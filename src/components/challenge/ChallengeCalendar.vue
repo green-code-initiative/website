3bf82027d2d23d58bd5fa5924b98b9d48b37b33b
utarwyn <maxime.malgorn@laposte.net>
2025-05-04 14:41:45 +0200

    Add challenge calendar

---
+++ src/components/challenge/ChallengeCalendar.vue
@@ -0,0 +1,146 @@
+<script lang="ts" setup>
+import ChallengeCalendarEvent from "@/components/challenge/ChallengeCalendarEvent.vue";
+import AppButton from "@/components/shared/AppButton.vue";
+import data from "@/assets/data/challenge-calendar.json";
+import { computed, ref } from "vue";
+
+const currentDateId = ref(data.dates[0].id);
+const events = computed(
+  () => data.dates.find((date) => date.id === currentDateId.value)?.events ?? []
+);
+</script>
+
+<template>
+  <div class="calendar">
+    <h2>Le planning</h2>
+    <div class="header">
+      <div class="caption">
+        <div v-for="eventType in data.eventTypes" :key="eventType.id">
+          <span class="event-badge" :class="eventType.id"></span>
+          <p>{{ eventType.name }}</p>
+        </div>
+      </div>
+      <div class="dates">
+        <AppButton
+          v-for="date in data.dates"
+          :key="date.id"
+          :variant="currentDateId === date.id ? 'secondary' : 'neutral'"
+          :text="date.title"
+          @click.prevent="currentDateId = date.id"
+        />
+      </div>
+    </div>
+    <div class="events">
+      <div v-for="(event, index) in events" class="event-timeline" :key="index">
+        <ChallengeCalendarEvent :event="event" />
+        <div class="children" v-if="event.children">
+          <ChallengeCalendarEvent
+            v-for="(childEvent, childIndex) in event.children"
+            :key="childIndex"
+            :child="true"
+            :event="childEvent"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.calendar {
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+
+  h2 {
+    font-size: 2rem;
+    font-weight: 800;
+    text-align: center;
+  }
+
+  .header {
+    display: flex;
+    flex-direction: column-reverse;
+    align-items: center;
+    gap: 2rem;
+
+    .caption {
+      display: flex;
+      flex-wrap: wrap;
+      gap: 1rem;
+      justify-content: center;
+
+      & > div {
+        display: flex;
+        align-items: center;
+        white-space: nowrap;
+        gap: 0.5rem;
+      }
+    }
+
+    .dates {
+      display: flex;
+      gap: 1rem;
+    }
+
+    @media (min-width: 960px) {
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+
+      .caption {
+        gap: 2rem;
+      }
+    }
+  }
+
+  .events {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+
+    .event-timeline {
+      display: flex;
+      flex-direction: column;
+      gap: 0.5rem;
+
+      .children {
+        display: flex;
+        flex-direction: column;
+        gap: 0.5rem;
+        position: relative;
+        margin-left: 2rem;
+
+        &:before {
+          content: "";
+          position: absolute;
+          left: -1rem;
+          top: -0.5rem;
+          width: 2px;
+          height: calc(100% + 1rem);
+          border-left: 2px dashed hsl(var(--neutral-300));
+        }
+      }
+    }
+  }
+
+  .event-badge {
+    width: 16px;
+    height: 16px;
+    border-radius: 50%;
+
+    &.teamwork {
+      background-color: hsl(var(--primary-500));
+    }
+    &.conference {
+      background-color: hsl(var(--secondary-500));
+    }
+    &.animation {
+      background-color: #e94242; // custom color for now
+    }
+    &.break {
+      border: 2px solid hsl(var(--neutral-400));
+    }
+  }
+}
+</style>
---
+++ src/components/challenge/ChallengeCalendarEvent.vue
@@ -0,0 +1,125 @@
+<script lang="ts" setup>
+defineProps<{ event: any; child?: boolean }>();
+</script>
+
+<template>
+  <div class="event" :class="{ child }">
+    <div class="type-and-time">
+      <div class="type">
+        <div class="event-badge" :class="event.type"></div>
+      </div>
+      <p class="starts-at">{{ event.startsAt }}</p>
+    </div>
+    <div class="content">
+      <h3>{{ event.title }}</h3>
+      <div v-if="event.speakers" class="speakers">
+        <div
+          v-for="(speaker, index) in event.speakers"
+          :key="index"
+          class="speaker"
+        >
+          <img
+            v-if="speaker.avatar"
+            :src="'/img/speaker/' + speaker.avatar"
+            width="40"
+            height="40"
+            :alt="speaker.name"
+          />
+          <div class="text">
+            <p>{{ speaker.name }}</p>
+            <p v-if="speaker.title" class="title">
+              {{ speaker.title }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <p v-if="event.duration" class="duration">{{ event.duration }}</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.event {
+  display: flex;
+  flex-direction: column;
+  align-items: baseline;
+  gap: 1rem;
+  padding: 1rem;
+  border-radius: 1rem;
+  border: 1px solid hsl(var(--neutral-300));
+
+  .type-and-time {
+    display: flex;
+    align-items: center;
+  }
+
+  .type {
+    width: 2rem;
+  }
+
+  .starts-at {
+    font-weight: 600;
+  }
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    flex-grow: 1;
+
+    h3 {
+      font-size: 1.25rem;
+      font-weight: 700;
+    }
+
+    .speakers {
+      display: flex;
+      flex-direction: column;
+      gap: 0.5rem;
+
+      .speaker {
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+        font-weight: 700;
+
+        img {
+          border-radius: 50%;
+        }
+
+        .title {
+          font-weight: 600;
+          color: hsl(var(--text-accent));
+        }
+      }
+    }
+  }
+
+  .duration {
+    font-weight: 600;
+    color: hsl(var(--text-accent));
+  }
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    gap: 4rem;
+
+    .type {
+      width: 5rem;
+    }
+
+    &.child .type {
+      width: 3rem;
+    }
+
+    .content {
+      gap: 1rem;
+    }
+
+    .speakers {
+      flex-direction: row;
+      gap: 1rem;
+    }
+  }
+}
+</style>
